843306de937d1ffb380a35bae9d4604652154cec
Sikita1 <ns310893@mail.ru>
2023-03-29 14:34:11 +0300

    /

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,7 +4,7 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] private float _speed;
 
-    void Update()
+    private void Update()
     {
         transform.position += Vector3.forward * _speed * Time.deltaTime;
     }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] private GameObject _template;
+    [SerializeField] private Spawner _enemy;
     [SerializeField] private Transform[] _spawnPoints;
     [SerializeField] private int _numberEnemies;
     [SerializeField] private float _interval;
@@ -20,7 +20,7 @@ public class Spawner : MonoBehaviour
         for (int i = 0; i < _numberEnemies; i++)
         {
             int randomPosition = Random.Range(minInclusive, _spawnPoints.Length);
-            Instantiate(_template, _spawnPoints[randomPosition].position, Quaternion.identity);
+            Instantiate(_enemy, _spawnPoints[randomPosition].position, Quaternion.identity);
             yield return new WaitForSeconds(_interval);
         }
     }
